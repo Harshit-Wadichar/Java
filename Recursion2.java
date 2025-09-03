db566cf7cd243c4e5eff17c0e948b1c71776ceb5
Harshit-Wadichar <manohar.wadichar9545@gmail.com>
2025-09-04 00:27:28 +0530

    added more code

--- Recursion2.java
+++ Recursion2.java
@@ -129,6 +129,23 @@ public class Recursion2 {
         uniqueSubsequences(str, idx + 1, newString, set);
     }
 
+    public static String[] keypad = { ".","abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
+
+    public static void printComb(String str, int idx, String combination) {
+        //to print the keypad combination
+        if (idx == str.length()) {
+            System.out.println(combination);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar - '0'];
+
+        for (int i = 0; i < mapping.length(); i++) {
+            printComb(str, idx + 1, combination+mapping.charAt(i));
+        }
+    }
+
     public static void main(String[] args){
         int n =3;
         towerOfHanoi(n, "S", "H", "D");
@@ -154,5 +171,8 @@ public class Recursion2 {
         String uSubsequence = "aaa";
         HashSet<String> set = new HashSet<>();
         uniqueSubsequences(uSubsequence, 0, "", set);
+
+        String keypadNo = "23";
+        printComb(keypadNo, 0, "");
     }
 }
